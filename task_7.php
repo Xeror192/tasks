6b06a24865bf98d80e3d52e76b90809a25fbdc52
Xeror <onikij4uk2015@yandex.ru>
2022-11-04 13:26:13 +0300

    seven task

--- task_7.php
+++ task_7.php
@@ -1,1 +1,11 @@
 <?php
+
+$firstNumber = 10;
+$secondNumber = 15;
+
+$sum = $firstNumber + $secondNumber;
+$subtraction = $firstNumber - $secondNumber;
+$multiplication = $firstNumber * $secondNumber;
+$division = $firstNumber / $secondNumber;
+
+echo $sum;
