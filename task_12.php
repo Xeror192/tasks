6bd3a80a1d3c6c384d70684e08425f6c343ec31d
Xeror <onikij4uk2015@yandex.ru>
2022-11-06 11:39:34 +0300

    eleven task

---
+++ task_12.php
@@ -0,0 +1,21 @@
+<?php
+
+class Warrior
+{
+    public $health;
+
+    public $stamina;
+
+    public function attack()
+    {
+        echo 'Воин атакует';
+    }
+    public function heal()
+    {
+        echo 'Здоровье война увеличено';
+    }
+    public function rest()
+    {
+        echo 'Воин присел восстановить силы';
+    }
+}
